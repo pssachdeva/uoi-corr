1bf34b52be64b33688e479de7b4fb2f4285569a2
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2018-06-21 09:47:11 -0700

    uoicorr

--- uoicorr.py
+++ uoicorr.py
@@ -11,36 +11,48 @@ from PyUoI.UoI_Lasso import UoI_Lasso
 ### parse arguments ###
 parser = argparse.ArgumentParser()
 parser.add_argument('--block_size', type=int, default=5)
+parser.add_argument('--n_blocks', type=int, default=5)
 parser.add_argument('--kappa', type=float, default=0.3)
 parser.add_argument('--reps', type=int, default=50)
+parser.add_argument('--sparsity', type=float, default=1.)
 parser.add_argument('--results_file', default='results.h5')
 args = parser.parse_args()
 
 block_size = args.block_size
+n_blocks = args.n_blocks
 kappa = args.kappa
 reps = args.reps
 results_file = args.results_file
 
-n_blocks = 5
+# set up other variables
 n_features = block_size * n_blocks
 n_samples = 5 * n_features
 
+# correlations and selection thresholds
 correlations = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 selection_thres_mins = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 
 results = h5py.File(results_file, 'w')
+# result arrays: fits
+betas = np.zeros((reps, n_features))
+beta_hats = np.zeros((reps, correlations.size, selection_thres_mins.size, n_features))
+# result arrays: scores
 fn_results = np.zeros((reps, correlations.size, selection_thres_mins.size))
+fp_results = np.zeros((reps, correlations.size, selection_thres_mins.size))
 r2_results = np.zeros((reps, correlations.size, selection_thres_mins.size))
-r2_true_results = np.zeros((reps, correlations.size, selection_thres_mins.size))
+r2_true_results = np.zeros((reps, correlations.size))
 
 for rep in range(reps):
 	beta = np.random.uniform(low=0, high=10, size=(n_features, 1))
+	mask = np.array([])
+	for 
 	for corr_idx, correlation in enumerate(correlations):
 		# create covariance matrix for block
 		block_Sigma = correlation * np.ones((block_size, block_size)) 
 		np.fill_diagonal(block_Sigma, np.ones(block_size))
 		# populate entire covariance matrix
-		Sigma = block_diag(block_Sigma, block_Sigma, block_Sigma, block_Sigma, block_Sigma)
+		rep_block_Sigma = [block_Sigma] * n_blocks
+		Sigma = block_diag(*rep_block_Sigma)
 		# draw samples
 		X = np.random.multivariate_normal(mean=np.zeros(n_features), cov=Sigma, size=n_samples)
 		X_test = np.random.multivariate_normal(mean=np.zeros(n_features), cov=Sigma, size=n_samples)
@@ -65,11 +77,14 @@ for rep in range(reps):
 			)
 			uoi.fit(X, y.ravel())
 			beta_hat = uoi.coef_
-			fn_results[rep, corr_idx, thres_idx] = np.count_nonzero(beta_hat == 0)
+			beta_hats[rep, corr_idx, thres_idx, :] = beta_hat
+			fn_results[rep, corr_idx, thres_idx] = np.count_nonzero(beta[beta_hat == 0])
+			fp_results[rep, corr_idx, thres_idx] = np.count_nonzero(beta_hat[beta == 0])
 			r2_results[rep, corr_idx, thres_idx] = r2_score(y_test, np.dot(X_test, beta_hat))
-			r2_true_results[rep, corr_idx, thres_idx] = r2_score(y_test, np.dot(X_test, beta))
 			print(time.time() - start)
+		r2_true_results[rep, corr_idx] = r2_score(y_test, np.dot(X_test, beta))
 results['fn'] = fn_results
+results['fp'] = fp_results
 results['r2'] = r2_results
 results['r2_true'] = r2_true_results
 results.close()
