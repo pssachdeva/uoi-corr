3544fd1dbc9c5b39ec733747aaeb68c010d9051f
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2018-06-23 10:42:23 -0700

    results

--- uoicorr_block.py
+++ uoicorr_block.py
@@ -18,19 +18,28 @@ parser.add_argument('--sparsity', type=float, default=1.)
 parser.add_argument('--results_file', default='results.h5')
 args = parser.parse_args()
 
+# size of each block
 block_size = args.block_size
+# number of blocks
 n_blocks = args.n_blocks
+# inverse signal-to-noise ratio
 kappa = args.kappa
+# sparsity of within block features
+sparsity = args.sparsity
+# number of repetitions
 reps = args.reps
+# filename for results
 results_file = args.results_file
 
 # set up other variables
 n_features = block_size * n_blocks
 n_samples = 5 * n_features
+n_nonzero_beta = int(sparsity * block_size)
 
 # correlations and selection thresholds
 correlations = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-selection_thres_mins = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+#selection_thres_mins = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+selection_thres_mins = np.array([0.5])
 
 results = h5py.File(results_file, 'w')
 # result arrays: fits
@@ -42,10 +51,17 @@ fp_results = np.zeros((reps, correlations.size, selection_thres_mins.size))
 r2_results = np.zeros((reps, correlations.size, selection_thres_mins.size))
 r2_true_results = np.zeros((reps, correlations.size))
 
-for rep in range(reps):
+for rep in range(1):
 	beta = np.random.uniform(low=0, high=10, size=(n_features, 1))
 	mask = np.array([])
-	for 
+	for block in range(n_blocks):
+		block_mask = np.zeros(block_size)
+		block_mask[:n_nonzero_beta] = np.ones(n_nonzero_beta)
+		np.random.shuffle(block_mask)
+		mask = np.concatenate((mask, block_mask))
+	mask = mask[..., np.newaxis]
+	beta = beta * mask
+	betas[rep, :] = beta.ravel()
 	for corr_idx, correlation in enumerate(correlations):
 		# create covariance matrix for block
 		block_Sigma = correlation * np.ones((block_size, block_size)) 
@@ -78,8 +94,8 @@ for rep in range(reps):
 			uoi.fit(X, y.ravel())
 			beta_hat = uoi.coef_
 			beta_hats[rep, corr_idx, thres_idx, :] = beta_hat
-			fn_results[rep, corr_idx, thres_idx] = np.count_nonzero(beta[beta_hat == 0])
-			fp_results[rep, corr_idx, thres_idx] = np.count_nonzero(beta_hat[beta == 0])
+			fn_results[rep, corr_idx, thres_idx] = np.count_nonzero(beta[beta_hat == 0, 0])
+			fp_results[rep, corr_idx, thres_idx] = np.count_nonzero(beta_hat[beta.ravel() == 0])
 			r2_results[rep, corr_idx, thres_idx] = r2_score(y_test, np.dot(X_test, beta_hat))
 			print(time.time() - start)
 		r2_true_results[rep, corr_idx] = r2_score(y_test, np.dot(X_test, beta))
@@ -87,6 +103,8 @@ results['fn'] = fn_results
 results['fp'] = fp_results
 results['r2'] = r2_results
 results['r2_true'] = r2_true_results
+results['beta'] = betas
+results['beta_hats'] = beta_hats
 results.close()
 
 
